e3b6040e6645b3a9b09e938efb249478d7dd9c93
apsdevx <75316606+apsdevx@users.noreply.github.com>
2022-09-10 12:26:57 +0530

    Add files via upload

---
+++ VS-Sample-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS_Sample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
